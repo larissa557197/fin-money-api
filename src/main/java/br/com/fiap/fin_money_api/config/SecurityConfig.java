4b38e8b2c938afe2eed20bc251a3465104e88194
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-15 11:43:18 -0300

    criamos application-pro.properties para colocar as credenciais do banco de dados da oracle

--- src/main/java/br/com/fiap/fin_money_api/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/fin_money_api/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package br.com.fiap.fin_money_api.config;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +8,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
